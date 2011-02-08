148a776d92084a450b8ddadba7485219e5cfe4ff
Milan Gardian <mgardian@gmail.com>
2011-02-08 15:36:18 -0700

    Pause at the end of build and clobber command-line scripts so that the window won't dissapear on double-click

--- p4.net_build.cmd
+++ p4.net_build.cmd
@@ -1,1 +1,2 @@
 @"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /nologo /maxcpucount /consoleloggerparameters:ShowCommandLine;ShowTimestamp /target:Build "%~dp0src/p4.net.proj"
+@pause
--- p4.net_clobber.cmd
+++ p4.net_clobber.cmd
@@ -1,1 +1,2 @@
 @"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /nologo /maxcpucount /consoleloggerparameters:ShowCommandLine;ShowTimestamp /target:Clobber "%~dp0src/p4.net.proj"
+@pause

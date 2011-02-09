de8fb75463b9ea590f95e1629ec758b7e593cc47
Milan Gardian <mgardian@gmail.com>
2011-02-08 23:46:52 -0700

    Added simple test project to load P4API assembly and force resolving of p4dn assembly

---
+++ src/P4API.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P4API.Test
+{
+
+    /// <summary>
+    /// </summary>
+    public static class Program
+    {
+
+        /// <summary>
+        /// </summary>
+        public static int Main(string[] args)
+        {
+            using (var c = new P4Connection())
+            {
+                c.Connect();
+            }
+
+            return 0;
+        }
+
+    }
+
+}
